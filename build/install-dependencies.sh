257b85fa0c0c5b55d448e834c88f0b5e2516096d
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-05-11 22:06:55 +0200

    Added a function to ensure that git is installed

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -15,14 +15,23 @@ apt-get install build-essential libboost-filesystem-dev libboost-system-dev libb
 
 
 # checkout aitools dependencies
+function ensure_git {
+    if ! [ -x "$(command -v git)" ]; then
+        echo "Installing git..."
+        apt-get install git -y
+    fi
+}
+
 mkdir -p ./dependencies
 cd ./dependencies
 if [ ! -d ./aitools3-aq-bighashmap-cpp ]; then
+    ensure_git
     git clone https://gitlab+deploy-token-8:52JyyPqQEkGg13es2qtb@git.webis.de/aitools/aitools3-aq-bighashmap-cpp.git
 else
     echo "aitools3-aq-bighashmap-cpp already present."
 fi
 if [ ! -d ./aitools3-aq-invertedindex3-cpp ]; then
+    ensure_git
     git clone https://gitlab+deploy-token-9:hxvTpSBa-WHExN5RZzey@git.webis.de/aitools/aitools3-aq-invertedindex3-cpp.git
 else
     echo "aitools3-aq-invertedindex3-cpp already present."
