77dd6956c78476c0d961b25efa5200d89b5e4bd1
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-05-11 19:15:43 +0200

    Updated messages of run-tests.sh script

--- build/run-tests.sh
+++ build/run-tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "asd"
-
 set -e
 
 # go to the dir of this script
 cd "$(dirname "$0")"
 
+echo "Rebuilding..."
 cd ..
 make
 
+echo "Running test..."
 cd build/release
 ./netspeak4-test
