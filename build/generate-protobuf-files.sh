69338ae3d274f0f4763c67ace1ec6b3ae79d18d9
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-06-03 15:28:35 +0200

    Updated proto files

--- build/generate-protobuf-files.sh
+++ build/generate-protobuf-files.sh
@@ -7,13 +7,12 @@ cd "$(dirname "$0")"
 
 # check protoc version
 protocVersion=$(protoc --version)
-if [ "$protocVersion" != "libprotoc 2.6.1" ]; then
-    echo "protoc v2.6.1 is required! Your version is $protocVersion";
-    # Netspeak might still work even with the wrong version.
-    # exit 1;
+if [ "$protocVersion" != "libprotoc 3.0.0" ]; then
+    echo "protoc v3.0.0 is required! Your version is $protocVersion";
+    exit 1;
 fi
 
 cd ../conf
 mkdir -p ../src/netspeak/generated
 rm -rf ../src/netspeak/generated/*
-protoc --cpp_out=../src/netspeak/generated ./NetspeakMessages.proto
+protoc --cpp_out=../src/netspeak/generated ./Netspeak_old.proto
