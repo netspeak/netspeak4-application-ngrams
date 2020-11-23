6b8b3c8cb2d79c81d58fa0061381d1177e113bf7
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-11-23 16:18:57 +0100

    Removed qt dependency

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -11,7 +11,7 @@ fi
 cd "$(dirname "$0")"
 
 apt-get update
-apt-get install make cmake cmake-data pkg-config qt4-default -y
+apt-get install make cmake cmake-data pkg-config -y
 apt-get install build-essential libboost-filesystem-dev libboost-system-dev libboost-test-dev libboost-program-options-dev libboost-stacktrace-dev libcmph-dev libaio-dev libboost-regex-dev -y
 
 
