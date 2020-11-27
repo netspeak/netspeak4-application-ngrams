75e7ca84698bc8e3447b1366fa3f6f440164b3fe
RunDevelopment <mitchi5000.ms@googlemail.com>
2020-11-27 13:03:41 +0100

    Compress test data

--- build/run-tests.sh
+++ build/run-tests.sh
@@ -9,5 +9,24 @@ echo "Rebuilding..."
 cd ..
 make
 
+
+# extract test files
+if [ ! -d ./test/data ]; then
+
+    if ! [ -x "$(command -v unzip)" ]; then
+        echo "Installing unzip..."
+        if (( $EUID != 0 )); then
+            sudo apt-get install -y unzip
+        else
+            apt-get install -y unzip
+        fi
+    fi
+
+    echo "Extracting test data"
+    unzip -q test/data.zip -d test/
+
+fi
+
+
 echo "Running test..."
 ./netspeak4-test --build_info=yes --detect_memory_leak=1 --log_level=test_suite
