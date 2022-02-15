44677f98e279ce0f62ffb2ae965e938f8facc661
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-02-15 16:07:15 +0100

    re-enable c++ tests

--- build/run-tests.sh
+++ build/run-tests.sh
@@ -29,7 +29,7 @@ fi
 
 
 echo "Running test..."
-# ./build/debug/netspeak4-test --build_info=yes --detect_memory_leak=1 --log_level=test_suite
+./build/debug/netspeak4-test --build_info=yes --detect_memory_leak=1 --log_level=test_suite
 
 echo "Running python tests..."
 PYTHONPATH="$(realpath ./build/debug):$PYTHONPATH" python3 ./test/py/test.py
